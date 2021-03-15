d604bc80d902a21a842f2a5f31c6dc6668f7023c
1XanXus0 <assaking@gmx.net>
2021-03-15 15:55:10 +0100

    tabellen auflistung filter system fin

--- tabellendaten_auflisten.php
+++ tabellendaten_auflisten.php
@@ -19,15 +19,26 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 // Query to get columns from table
 $query = $link->query("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = 'mydb' AND TABLE_NAME = '$tabellenName'");
 
-
+//output variablen
 $outputColNames = '';
 $output = '<tr>';
 $outputCheckBox = '';
+$outputTextField = '';
+$outputComboBox = "	<div class='col-md-12'>
+						<span>Sortieren</span>
+						<div class='row'>
+							<div class='col-md-8'>
+								<select class='form-control' name='list'>";
+
 $checkedColArr = array();
 $isChecked = 'checked';
 $count = 0;
 $whereStmt = '';
+$sortStmt = '';
 
+$filterValue1 = '';
+$filterValue2 = '';
+$selectedValue = '';
 
 while($row = $query->fetch_assoc()){
     $result[] = $row;
@@ -48,12 +59,57 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 	}
 	else{
 		$checkedColArr = $columnArr;
-	}	
+	}
+	
+	if($tabellenName == "Standortdaten" && !empty($_POST["bezeichnung"]))
+	{
+		
+		$whereStmt = "WHERE Bez_der_Tageseinrichtung = '" .$_POST["bezeichnung"] . "'";
+		$filterValue1 = $_POST['bezeichnung'];
+	}
+	else if(!empty($_POST["vorname"]) && !empty($_POST["nachname"]))
+	{
+		$whereStmt = "WHERE Vorname = '" .$_POST["vorname"] . "'AND Nachname =  '" . $_POST["nachname"] . "'";
+		$filterValue1 = $_POST['vorname'];
+		$filterValue2 = $_POST['nachname'];
+	}
+	else if(!empty($_POST["vorname"]))
+	{
+		$whereStmt = "WHERE Vorname = '" .$_POST["vorname"] . "'";
+		$filterValue1 = $_POST['vorname'];
+	}
+	else if(!empty($_POST["nachname"]))
+	{
+		$whereStmt = "WHERE Nachname = '" .$_POST["nachname"] . "'";
+		$filterValue2 = $_POST['nachname'];
+	}
 
+	if(!empty($_POST["list"]))
+	{
+		if(!empty($_POST["radio"]))
+		{
+			if($_POST["radio"] == "DESC")
+			{
+				$sortStmt = "ORDER BY " .$_POST['list'] . " DESC"; 				
+			} else {
+				$sortStmt = "ORDER BY " .$_POST['list'] . " ASC";
+			}
+		} 
+		else if(empty($_POST["radio"]))
+		{
+			$sortStmt = "ORDER BY " .$_POST['list'] . " ASC"; 	
+		}
+		
+	}
+		
+		
+}
+else{
+	$checkedColArr = $columnArr;
 }
 
-// Generating table header 
-$sql = "SELECT * FROM $tabellenName $whereStmt";
+// Generating table header
+$sql = "SELECT * FROM $tabellenName $whereStmt $sortStmt";
 $result = mysqli_query($link, $sql);
 
 foreach($checkedColArr as $key => $var)
@@ -72,7 +128,7 @@ if (mysqli_num_rows($result) > 0) {
     }
 } 
 
-// Creating the Checkboxres
+// Creating the Checkboxes
 foreach($columnArr as $key => $var)
 {
 	if(in_array("$var", $checkedColArr) || empty($checkedColArr))
@@ -91,10 +147,74 @@ foreach($columnArr as $key => $var)
 		$outputCheckBox .= "<div class='row'>";
 	}
 	$count = $count + 1;
-	$outputCheckBox .= "<div class='col-md-1'><label class='fancy-checkbox'><input type='checkbox' name='check_list[]' value='$var' $isChecked><span>" .$var ."</span></label></div>";
+	$outputCheckBox .= "<div class='col-md-2'>
+							<label class='fancy-checkbox'>
+								<input type='checkbox' name='check_list[]' value='$var' $isChecked>
+								<span>" .$var ."</span>
+							</label>
+						</div>";
 }
 if($count != 6 || $count != 0)
+{
 	$outputCheckBox .= "</div>";
+}
+
+
+// Creating searching fields
+if($tabellenName == "Standortdaten")
+{
+	$outputTextField = "<div class='col-md-12'>
+							<span>Standortbezeichnung</span>
+							<input type='text' name='bezeichnung' class='form-control' value='$filterValue1'>
+							<span class='help-block'><br></span>
+						</div>";
+}
+else {
+	$outputTextField = "<div class='col-md-6'>
+							<span>Vorname</span>
+							<input type='text' name='vorname' class='form-control' value='$filterValue1'>
+							<span class='help-block'><br></span>
+						</div>
+						<div class='col-md-6'>
+							<span>Nachname</span>
+							<input type='text' name='nachname' class='form-control' value='$filterValue2'>
+							<span class='help-block'><br></span>
+						</div>";
+}
+
+//Creating ComboBox for sorting
+
+foreach($columnArr as $key => $var)
+{
+	if(!empty($_POST['list']))
+	{
+		if($var == $_POST["list"])
+		{
+			$selectedValue = "selected";
+		}
+		else {
+			$selectedValue = "";
+		}
+	}
+	$outputComboBox .= "<option value='$var' $selectedValue>$var</option>";
+}
+$outputComboBox .= "</select>
+					</div>
+					<div class='col-md-1'>
+						<label class='fancy-radio'>
+							<input name='radio' value='ASC' type='radio'>
+							<span><i></i>ASC</span>
+						</label>
+					</div>
+					<div class='col-md-1'>
+						<label class='fancy-radio'>
+							<input name='radio' value='DESC' type='radio'>
+							<span><i></i>DESC</span>
+						</label>
+					</div>
+					</div>
+					<span class='help-block'><br></span>	
+					</div>";
 
 ?>
 
@@ -219,7 +339,9 @@ if($count != 6 || $count != 0)
                         <button type="submit" name="dbName" class="btn btn-primary btn-lg" value="Mitarbeiterdaten">Mitarbeiterdaten</button>
                         <button type="submit" name="dbName" class="btn btn-primary btn-lg" value="Standortdaten">Standortdaten</button>						
 					</form>
-					<!-- PANEL NO PADDING -->
+					<div class="row">
+						<!-- PANEL NO PADDING -->
+						<div class='col-md-10'>
 							<div class="panel">
 								<div class="panel-heading">
 									<h5 class="panel-title">Filter Optionen</h3>
@@ -227,22 +349,35 @@ if($count != 6 || $count != 0)
 										<button type="button" class="btn-toggle-collapse"><i class="lnr lnr-chevron-up"></i></button>
 									</div>
 								</div>
-								<div class="panel-body no-padding">
+								<div class="panel-body no-padding">								
 									<div class="padding-top-10">
 									<form class="form-auth-small" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
-									<div class="filter">
+									<div class="row">
 										<div class='col-md-10'>
 											<?php echo $outputCheckBox; ?>
-										</div>
-										<div class='col-md-10'>
-											<?php  ?>
+										</div>										
+									</div>
+									<div class="row">	
+										<div class='col-md-10'>					
+											<?php  echo $outputTextField; ?>
+										</div>	
+									</div>
+									<div class="row">	
+										<div class='col-md-10'>					
+											<?php echo $outputComboBox; ?>
 										</div>
 									</div>
+										
+														
 										<button type="submit" name="dbName" class="btn btn-primary btn-block" value="<?php echo $tabellenName; ?>">Filtern</button>
+									
+										
 									</form>
-									</div>
+									</div>								
 								</div>
 							</div>
+						</div>
+					</div>
 							<!-- END PANEL NO PADDING -->
                     
 							<!-- TABLE HOVER -->
@@ -250,6 +385,7 @@ if($count != 6 || $count != 0)
 								<div class="panel-heading">
 									<h3 class="panel-title">Auflistung</h3>
 								</div>
+								<div class="tabellen">
 								<div class="panel-body">
 									<table class="table table-hover">
 										<thead>
@@ -263,7 +399,9 @@ if($count != 6 || $count != 0)
 									</table>
 								</div>
 							</div>
+							</div>
 							<!-- END TABLE HOVER -->
+						
 				</div>
 			</div>
 			<!-- END MAIN CONTENT -->
